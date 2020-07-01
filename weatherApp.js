82d2b5b1ef8f4e7ca74c068a9f04f3ba4c723d6b
bwaterman8 <bwaterman8@gmail.com>
2020-06-30 21:00:39 -0400

    fetched location and temperature

--- weatherApp.js
+++ weatherApp.js
@@ -1,3 +1,19 @@
-fetch(`http://api.openweathermap.org/data/2.5/weather?zip=43214,us&appid=02ed7e7d8b698b88dab20bc8f8e848ad`)
-.then(respond => respond.json())
-.then(data => console.log(data))
+window.addEventListener('load', ()=> {
+    let weatherLocation = document.querySelector('.the_location')
+    let weatherIcon = document.querySelector('.icon')
+    let weatherTemp = document.querySelector('.the_temp')
+    let weatherStyle = document.querySelector('.the_style')
+
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=43214,us&units=imperial&appid=02ed7e7d8b698b88dab20bc8f8e848ad`)
+    .then(respond => respond.json())
+    .then(data => {
+        console.log(data);
+        const {name} = data
+        weatherLocation.textContent = name
+        const {temp} = data.main
+        weatherTemp.textContent = temp
+        const {style} = data.weather
+        weatherStyle.textContent = style
+    })
+})
