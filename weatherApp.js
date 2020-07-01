773059af4e93fe2794531d147903eaace1b47156
bwaterman8 <bwaterman8@gmail.com>
2020-07-01 10:24:45 -0400

    fetched weather description

--- weatherApp.js
+++ weatherApp.js
@@ -2,18 +2,19 @@ window.addEventListener('load', ()=> {
     let weatherLocation = document.querySelector('.the_location')
     let weatherIcon = document.querySelector('.icon')
     let weatherTemp = document.querySelector('.the_temp')
-    let weatherStyle = document.querySelector('.the_style')
+    let weatherStyle = document.querySelector('.the_weather')
 
 
     fetch(`http://api.openweathermap.org/data/2.5/weather?zip=43214,us&units=imperial&appid=02ed7e7d8b698b88dab20bc8f8e848ad`)
     .then(respond => respond.json())
     .then(data => {
-        console.log(data);
-        const {name} = data
+        // console.log(data);
+        let name = data['name']
         weatherLocation.textContent = name
-        const {temp} = data.main
+        let temp = data['main']['temp']
         weatherTemp.textContent = temp
-        const {style} = data.weather
-        weatherStyle.textContent = style
+        let weatherDesc = data['weather'][0]['description']
+        weatherStyle.textContent = weatherDesc
     })
 })
+// ['weather'][0]['description']
